b66e6f56c8402c846d00cbf5e8aedf6ae5fef679
chenang <blogca@qq.com>
2022-04-27 10:47:34 +0800

    48. 旋转图像

---
+++ 力扣刷题top100/力扣48-旋转图像.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        //先上下交换，后主对角线交换
+        for (int i = 0; i < n / 2; ++i)
+            for (int j = 0; j < n; ++j)
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
+
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                swap(matrix[i][j], matrix[j][i]);
+
+        return;
+    }
+};
+
+int main() {
+
+	system("pause");
+	return 0;
+}
