82ce83f4bc78770a594a506379246a341d4354bb
chenang <blogca@qq.com>
2022-05-04 13:19:23 +0800

    141. 环形链表

---
+++ 力扣刷题top100/力扣141-环形链表.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+   int val;
+   ListNode* next;
+   ListNode(int x) : val(x), next(NULL) {}
+   
+};
+ //¿ìÂıÖ¸Õë
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == NULL) return false;
+        if (head->next == head) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+
+	system("pause");
+	return 0;
+}
